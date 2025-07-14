7b1cfba5848be5a73fc4a12598a1b1fb0b39387d
hokjanbaruda <hokjanbaruda@gmail.com>
2025-07-14 21:54:25 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bocil Bo';
-export const desk = 'Video Bokep Indo Jepang Jav Porn Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma';
-export const url = 'https://bocilbo.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
